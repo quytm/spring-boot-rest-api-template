9e638d8cf539e0707450474bcea356be2c46eb16
quytm <minhquylt95@gmail.com>
2019-07-31 19:05:38 +0700

    init project

---
+++ src/main/java/com/tmq/food4u/Food4uApplication.java
@@ -0,0 +1,13 @@
+package com.tmq.food4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Food4uApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Food4uApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmq/food4u/Food4uApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tmq.food4u;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Food4uApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
